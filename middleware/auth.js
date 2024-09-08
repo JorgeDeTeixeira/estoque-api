6693fd56f4fe751de2057e368ffe4bd986f7806f
Jorge Teixeira <jorgedeteixeirademelo@gmail.com>
2024-09-08 10:10:21 -0300

    Add authentication middleware with token verification and admin role check

---
+++ middleware/auth.js
@@ -0,0 +1,41 @@
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const secret = process.env.JWT_SECRET;
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Token não informado" });
+  }
+
+  // Verifica se o token é válido
+  jwt.verify(token, secret, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: "Token inválido" });
+    }
+    req.user = user;
+    next();
+  });
+}
+
+// Middleware para verificar se  o usuário é admin
+function isAdmin(req, res, next) {
+  // Verifica se o usuário foi definido
+  if (!req.user) {
+    return res.status(403).json({ message: "User not authenticated" });
+  }
+
+  //Verifica o papel do usuário
+  if (req.user.role === "admin") {
+    next(); // Continua se for admin
+  } else {
+    return res.status(403).json({ message: "Access denied. Admins only" });
+  }
+}
+
+module.exports = { authenticateToken, isAdmin };
