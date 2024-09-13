213b69be6d670a20e0d5ba7defd764a3c87646bd
Jorge Teixeira <jorgedeteixeirademelo@gmail.com>
2024-09-13 11:09:33 -0300

    feat: Refatoração do auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,6 +5,11 @@ dotenv.config();
 
 const secret = process.env.JWT_SECRET;
 
+if (!secret) {
+  throw new Error("JWT_SECRET environment variable is not set");
+}
+
+// Middleware para autenticação de token
 function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
@@ -13,7 +18,6 @@ function authenticateToken(req, res, next) {
     return res.status(401).json({ message: "Token não informado" });
   }
 
-  // Verifica se o token é válido
   jwt.verify(token, secret, (err, user) => {
     if (err) {
       return res.status(403).json({ message: "Token inválido" });
@@ -23,18 +27,21 @@ function authenticateToken(req, res, next) {
   });
 }
 
-// Middleware para verificar se  o usuário é admin
+// Middleware para verificar se o usuário é admin
 function isAdmin(req, res, next) {
-  // Verifica se o usuário foi definido
   if (!req.user) {
-    return res.status(403).json({ message: "User não autenticado" });
+    return res.status(403).json({ message: "Usuário não autenticado" });
   }
 
-  //Verifica o papel do usuário
   if (req.user.role === "admin") {
-    next(); // Continua se for admin
+    next();
   } else {
-    return res.status(403).json({ message: "Acesso negado. Apenas usúarios autorizados" });
+    return res
+      .status(403)
+      .json({
+        message:
+          "Acesso negado. Apenas administradores podem acessar esta rota",
+      });
   }
 }
 
