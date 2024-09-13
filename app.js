a2ec2e0542ccf1e00c0c51c0497bd2223ca434a6
Jorge Teixeira <jorgedeteixeirademelo@gmail.com>
2024-09-13 11:10:14 -0300

    feat: refatoração do app.js

--- app.js
+++ app.js
@@ -2,19 +2,33 @@ const express = require("express");
 const dotenv = require("dotenv");
 const userRoutes = require("./routes/userRoutes");
 const estoqueRoutes = require("./routes/estoqueRoutes");
-const itensRoutes = require("./routes/itemRoutes");
+const itemRoutes = require("./routes/itemRoutes");
 
 dotenv.config();
 
 const app = express();
 
-// Middleware para tratar o Json
+// Middleware para tratar o JSON
 app.use(express.json());
+
+// Middleware de logging (opcional)
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
 
 // Rotas
 app.use("/api/users", userRoutes);
 app.use("/api/estoques", estoqueRoutes);
-app.use("/api/itens", itensRoutes);
+app.use("/api/itens", itemRoutes);
+
+// Middleware para tratamento de erros
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res
+    .status(500)
+    .json({ message: "Internal Server Error", error: err.message });
+});
 
 // Inicia o servidor
 const PORT = process.env.PORT || 3000;
